a8b98f7b92c53ef6d14b1417945f85580683a8a9
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2020-06-18 15:22:04 +0530

    🚀 project completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import arrayWords from '../utils/words';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'word-generator';
+
+  words: string = '';
+  limit: number = 10;
+
+  handleSlideChange(newLimit: number) {
+    this.limit = newLimit;
+  }
+
+  generateWords() {
+    this.words = arrayWords.slice(0, this.limit).join(' ');
+  }
+
+  generateRandomNum() {
+    this.limit = Math.floor(Math.random() * 50 + 1);
+  }
 }
